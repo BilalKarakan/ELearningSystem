23cd4d0d5f7befb92ea9d68fed069a469ebfef5c
BilalKarakan <bilalkaraka36@gmail.com>
2025-09-07 17:09:13 +0300

    Add project files.

---
+++ src/ELearningSystem.WebUI/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ELearningSystem.WebUI.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}

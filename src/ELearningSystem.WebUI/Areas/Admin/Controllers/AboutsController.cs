1d43a3b371f9cb63f9e336f1ee735d1ef206b411
BilalKarakan <bilalkaraka36@gmail.com>
2025-09-16 18:28:39 +0300

    The template has been added to the application. Edits have been made.

---
+++ src/ELearningSystem.WebUI/Areas/Admin/Controllers/AboutsController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ELearningSystem.WebUI.Areas.Admin.Controllers;
+
+[Area("Admin")]
+[Route("[area]/[controller]/[action]/{id?}")]
+public class AboutsController : Controller
+{
+    public IActionResult Index()
+    {
+        return View();
+    }
+}
